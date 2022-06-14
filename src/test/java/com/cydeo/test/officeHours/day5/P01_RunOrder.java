226bd36cc3be0eb53f40c9611522f4f30b9f63c7
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-06-14 17:41:12 -0400

    day05_selenium

---
+++ src/test/java/com/cydeo/test/officeHours/day5/P01_RunOrder.java
@@ -0,0 +1,27 @@
+package com.cydeo.test.officeHours.day5;
+
+import org.testng.annotations.Test;
+
+public class P01_RunOrder {
+    @Test
+    public void dtest1() {
+        System.out.println("test1");
+    }
+
+    @Test
+    public void atest2() {
+        System.out.println("test2");
+
+    }
+
+    @Test
+    public void btest3() {
+        System.out.println("test3");
+
+    }
+    @Test
+    public void ctest4() {
+        System.out.println("test4");
+
+    }
+}
